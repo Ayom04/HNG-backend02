4ad9b9df611590584e22a7dd4d928a7878298dc1
Ayom04 <abdullahayomide04@gmail.com>
2024-07-06 08:29:48 +0100

    fix(express) version

--- index.ts
+++ index.ts
@@ -3,12 +3,15 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import messages from "./src/constants/messages";
 import response from "./src/utils/response";
 import cors from "cors"
-const app = express();
-
+// const app = express();
+import {HttpError} from "./src/utils/httpResponse"
+const app: Express = express();
+import authRoute from "./src/routes/user"
 const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   return response(res, 200, messages.welcomeMessage);
 });
@@ -16,11 +19,12 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
+app.use('/auth', authRoute)
 
-app.use(function (err, req:Request, res:Response, next:NextFunction) {
+app.use(function (err:Response, req:Request, res:Response, next:NextFunction) {
   console.log(err)
   //check if it is database error, if yes, dont display the postgre error
-  if (err.client in err && err.client === 'postre') {
+  if (err.client === 'postre') {
     return response(res,500,'Something went wrong. Kindly try again later.')
     ;
   }
